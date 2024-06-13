50f32efc4b9df44221ad8387049f75300202b545
Mike O'Brien <mike.j.obrien@gmail.com>
2024-06-13 14:25:23 -0700

    simplify tests

--- tests/unit/test_remove_element.py
+++ tests/unit/test_remove_element.py
@@ -1,74 +1,63 @@
 from src.remove_element import removeElement
 from src.remove_element import removeElementTwoPointers
 
-
 def test_remove_element_one_occurance():
     nums = [3, 2, 2]
-    remove = 3
-    nums_expected = [2, 2]
-    r = removeElement(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  3
+    k = removeElement(nums, val)
+    assert sorted(nums[:k]) == [2, 2]
 
 def test_remove_element_two_pointers_one_occurance():
     nums = [3, 2, 2]
-    remove = 3
-    nums_expected = [2, 2]
-    r = removeElementTwoPointers(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  3
+    k = removeElementTwoPointers(nums, val)
+    assert sorted(nums[:k]) == [2, 2]
 
 def test_remove_element_multiple_occurnaces():
     nums = [3, 2, 2, 3]
-    remove = 3
-    nums_expected = [2, 2]
-    r = removeElement(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  3
+    k = removeElement(nums, val)
+    assert sorted(nums[:k]) == [2, 2]
 
 def test_remove_element_two_pointers_multiple_occurnaces():
     nums = [3, 2, 2, 3]
-    remove = 3
-    nums_expected = [2, 2]
-    r = removeElementTwoPointers(nums, remove)
-    assert sorted(nums[:r]) == nums_expected    
+    val =  3
+    k = removeElementTwoPointers(nums, val)
+    assert sorted(nums[:k]) == [2, 2]  
 
 def test_remove_element_empty_array():
     nums = []
-    remove = 1
-    nums_expected = []
-    r = removeElement(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  1
+    k = removeElement(nums, val)
+    assert sorted(nums[:k]) == []
 
 def test_remove_element_two_pointers_empty_array():
     nums = []
-    remove = 1
-    nums_expected = []
-    r = removeElementTwoPointers(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  1
+    k = removeElementTwoPointers(nums, val)
+    assert sorted(nums[:k]) == []
 
 def test_remove_element_no_occurance():
     nums = [1, 2, 3, 4, 5]
-    remove = 6
-    nums_expected = [1, 2, 3, 4, 5]
-    r = removeElement(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  6
+    k = removeElement(nums, val)
+    assert sorted(nums[:k]) == [1, 2, 3, 4, 5]
 
 def test_remove_element_two_pointers_no_occurance():
     nums = [1, 2, 3, 4, 5]
-    remove = 6
-    nums_expected = [1, 2, 3, 4, 5]
-    r = removeElementTwoPointers(nums, remove)
-    assert sorted(nums[:r]) == nums_expected    
+    val =  6
+    k = removeElementTwoPointers(nums, val)
+    assert sorted(nums[:k]) == [1, 2, 3, 4, 5]
 
 def test_remove_element_all():
     nums = [1, 1, 1]
-    remove = 1
-    nums_expected = []
-    r = removeElement(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  1
+    k = removeElement(nums, val)
+    assert sorted(nums[:k]) == []
 
 def test_remove_element_two_pointers_all():
     nums = [1, 1, 1]
-    remove = 1
-    nums_expected = []
-    r = removeElementTwoPointers(nums, remove)
-    assert sorted(nums[:r]) == nums_expected
+    val =  1
+    k = removeElementTwoPointers(nums, val)
+    assert sorted(nums[:k]) == []
    
