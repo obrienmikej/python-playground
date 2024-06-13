2932716c874af0a49d176e48a18882425cf98160
Mike O'Brien <mike.j.obrien@gmail.com>
2024-06-13 14:34:37 -0700

    DRY: use pytest function to combine tests

--- tests/unit/test_remove_element.py
+++ tests/unit/test_remove_element.py
@@ -1,62 +1,38 @@
+import pytest
 from src.remove_element import removeElement, removeElementTwoPointers
 
-def test_remove_element_one_occurance():
+@pytest.mark.parametrize("func", [removeElement, removeElementTwoPointers])
+def test_remove_element_one_occurance(func):
     nums = [3, 2, 2]
     val =  3
-    k = removeElement(nums, val)
+    k = func(nums, val)
     assert sorted(nums[:k]) == [2, 2]
 
-def test_remove_element_two_pointers_one_occurance():
-    nums = [3, 2, 2]
-    val =  3
-    k = removeElementTwoPointers(nums, val)
-    assert sorted(nums[:k]) == [2, 2]
-
-def test_remove_element_multiple_occurnaces():
+@pytest.mark.parametrize("func", [removeElement, removeElementTwoPointers])
+def test_remove_element_multiple_occurnaces(func):
     nums = [3, 2, 2, 3]
     val =  3
-    k = removeElement(nums, val)
+    k = func(nums, val)
     assert sorted(nums[:k]) == [2, 2]
 
-def test_remove_element_two_pointers_multiple_occurnaces():
-    nums = [3, 2, 2, 3]
-    val =  3
-    k = removeElementTwoPointers(nums, val)
-    assert sorted(nums[:k]) == [2, 2]  
-
-def test_remove_element_empty_array():
+@pytest.mark.parametrize("func", [removeElement, removeElementTwoPointers])
+def test_remove_element_empty_array(func):
     nums = []
     val =  1
-    k = removeElement(nums, val)
+    k = func(nums, val)
     assert sorted(nums[:k]) == []
 
-def test_remove_element_two_pointers_empty_array():
-    nums = []
-    val =  1
-    k = removeElementTwoPointers(nums, val)
-    assert sorted(nums[:k]) == []
-
-def test_remove_element_no_occurance():
+@pytest.mark.parametrize("func", [removeElement, removeElementTwoPointers])
+def test_remove_element_no_occurance(func):
     nums = [1, 2, 3, 4, 5]
     val =  6
-    k = removeElement(nums, val)
+    k = func(nums, val)
     assert sorted(nums[:k]) == [1, 2, 3, 4, 5]
 
-def test_remove_element_two_pointers_no_occurance():
-    nums = [1, 2, 3, 4, 5]
-    val =  6
-    k = removeElementTwoPointers(nums, val)
-    assert sorted(nums[:k]) == [1, 2, 3, 4, 5]
-
-def test_remove_element_all():
+@pytest.mark.parametrize("func", [removeElement, removeElementTwoPointers])
+def test_remove_element_all(func):
     nums = [1, 1, 1]
     val =  1
-    k = removeElement(nums, val)
-    assert sorted(nums[:k]) == []
-
-def test_remove_element_two_pointers_all():
-    nums = [1, 1, 1]
-    val =  1
-    k = removeElementTwoPointers(nums, val)
+    k = func(nums, val)
     assert sorted(nums[:k]) == []
    
