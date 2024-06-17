619a4eb3499e8b6cd196f88ea06c51812b094206
Mike O'Brien <mike.j.obrien@gmail.com>
2024-06-17 11:44:34 -0700

    add tests

--- tests/unit/test_plus_one.py
+++ tests/unit/test_plus_one.py
@@ -9,7 +9,6 @@ class TestPlusOne(unittest.TestCase):
         self.assertEqual(plusOne([1]), [2])
         self.assertEqual(plusOne([9]), [1, 0])
 
-
     def test_multiple_digits(self):
         self.assertEqual(plusOne([1, 2, 3]), [1, 2, 4])
         self.assertEqual(plusOne([4, 3, 2, 1]), [4, 3, 2, 2])
@@ -17,11 +16,11 @@ class TestPlusOne(unittest.TestCase):
         self.assertEqual(plusOne([2, 9, 9]), [3, 0, 0])
         self.assertEqual(plusOne([9, 8, 9]), [9, 9, 0])
 
-# def test_large_numbers(self):
-#     self.assertEqual(plusOne([9] * 100), [1] + [0] * 100)
-#     self.assertEqual(plusOne([1] + [0] * 99), [1] + [0] * 98 + [1])
+    def test_large_numbers(self):
+        self.assertEqual(plusOne([9] * 100), [1] + [0] * 100)
+        self.assertEqual(plusOne([1] + [0] * 99), [1] + [0] * 98 + [1])
 
-# def test_edge_cases(self):
-#     self.assertEqual(plusOne([0]), [1])
-#     self.assertEqual(plusOne([8, 9, 9, 9]), [9, 0, 0, 0])
-#     self.assertEqual(plusOne([1, 0, 0, 0]), [1, 0, 0, 1])
+    def test_edge_cases(self):
+        self.assertEqual(plusOne([0]), [1])
+        self.assertEqual(plusOne([8, 9, 9, 9]), [9, 0, 0, 0])
+        self.assertEqual(plusOne([1, 0, 0, 0]), [1, 0, 0, 1])
